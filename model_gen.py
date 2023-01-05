2b4bdd34a2196437fdc7226fe29b93aec70e1f09
nexhua <CChristofear@hotmail.com>
2023-01-05 22:05:24 +0300

    changes to the model

--- model_gen.py
+++ model_gen.py
@@ -26,6 +26,23 @@ class Model:
 
         return model
 
+    def create_model_v2(self):
+        model = models.Sequential()
+
+        model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                  input_shape=self.config.INPUT_DIMS))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+        model.add(layers.MaxPooling2D((2, 2)))
+        model.add(layers.Flatten())
+        model.add(layers.Dense(512, activation='relu'))
+        model.add(layers.Dense(256, activation='relu'))
+        model.add(layers.Dense(self.config.DIM))
+
+        return model
+
     def create_model_with_vgg16(self):
         conv_base = VGG16(weights="imagenet", include_top=False,
                           input_shape=self.config.INPUT_DIMS)
@@ -40,5 +57,4 @@ class Model:
         return model
 
     def compile(self, model):
-        model.compile(loss='mean_squared_error', optimizer='adam', metrics=[
-                      'mean_absolute_error', 'mean_squared_error'])
+        model.compile(loss='mean_absolute_error', optimizer='adam')
