e0faa672d531628760df0bc71b50e5f4d40117b8
nexhua <CChristofear@hotmail.com>
2023-01-06 04:24:05 +0300

    minor

--- model_gen.py
+++ model_gen.py
@@ -18,10 +18,9 @@ class Model:
         model.add(layers.MaxPooling2D((2, 2)))
         model.add(layers.Conv2D(128, (3, 3), activation='relu'))
         model.add(layers.MaxPooling2D((2, 2)))
-        model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-        model.add(layers.MaxPooling2D((2, 2)))
         model.add(layers.Flatten())
         model.add(layers.Dense(512, activation='relu'))
+        model.add(layers.Dense(256, activation='relu'))
         model.add(layers.Dense(self.config.DIM))
 
         return model
@@ -34,11 +33,9 @@ class Model:
         model.add(layers.MaxPooling2D((2, 2)))
         model.add(layers.Conv2D(64, (3, 3), activation='relu'))
         model.add(layers.MaxPooling2D((2, 2)))
-        model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-        model.add(layers.MaxPooling2D((2, 2)))
         model.add(layers.Flatten())
+        model.add(layers.Dropout(0.5))
         model.add(layers.Dense(512, activation='relu'))
-        model.add(layers.Dense(256, activation='relu'))
         model.add(layers.Dense(self.config.DIM))
 
         return model
