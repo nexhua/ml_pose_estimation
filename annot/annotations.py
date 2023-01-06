d4881bc34438b51b0b45d02dbbe3ea65b60da59d
nexhua <CChristofear@hotmail.com>
2023-01-06 10:51:50 +0300

    changed absolute file paths to relative

--- annot/annotations.py
+++ annot/annotations.py
@@ -10,25 +10,25 @@ class Data:
         pass
 
     def get_train_data(self):
-        TRAIN_JSON_DIR = "C:\\Users\\kocam\\Projects\\Datascience\\Projects\\ml_pose_estimation\\annot\\train.json"
+        TRAIN_JSON_DIR = "annot\\train.json"
 
         file = open(TRAIN_JSON_DIR)
         return json.load(file)
 
     def get_validation_data(self):
-        VALIDATION_JSON_DIR = "C:\\Users\\kocam\\Projects\\Datascience\\Projects\\ml_pose_estimation\\annot\\trainval.json"
+        VALIDATION_JSON_DIR = "annot\\trainval.json"
 
         file = open(VALIDATION_JSON_DIR)
         return json.load(file)
 
     def get_test_data(self):
-        TEST_JSON_DIR = "C:\\Users\\kocam\\Projects\\Datascience\\Projects\\ml_pose_estimation\\annot\\test.json"
+        TEST_JSON_DIR = "annot\\test.json"
 
         file = open(TEST_JSON_DIR)
         return json.load(file)
 
     def get_valid(self):
-        VALID_JSON_DIR = "C:\\Users\\kocam\\Projects\\Datascience\\Projects\\ml_pose_estimation\\annot\\valid.json"
+        VALID_JSON_DIR = "annot\\valid.json"
 
         file = open(VALID_JSON_DIR)
         return json.load(file)
--- configurations.py
+++ configurations.py
@@ -5,9 +5,9 @@ Config file for the entire Machine Learning Project
 
 class Configuration:
     def __init__(self):
-        self.IMAGES_BASE_DIR = "C:\\Users\\kocam\\Projects\\Datascience\\Projects\\ml_pose_estimation\\images"
-        self.ANNOTATIONS_BASE_DIR = "C:\\Users\\kocam\\Projects\\Datascience\\Projects\\ml_pose_estimation\\annot"
-        self.ORIGINAL_DATASET_BASE_DIR = "C:\\Users\\kocam\\Projects\\Datascience\\Projects\\ml_pose_estimation\\images\\all\\images"
+        self.IMAGES_BASE_DIR = "images"
+        self.ANNOTATIONS_BASE_DIR = "annot"
+        self.ORIGINAL_DATASET_BASE_DIR = "images\\all\\images"
         self.KEYPOINT_COUNT = 16
         self.DIM = self.KEYPOINT_COUNT * 2 + 2
         self.INPUT_DIMS = (224, 224, 3)
